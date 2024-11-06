f9ff546747b29d1cea608da83482d9d338c14072
Saliim242 <salim7442a@gmail.com>
2024-11-06 08:45:35 +0300

    Updated Auth Controller file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,5 @@
 import bcryptjs from "bcryptjs";
 import crypto from "crypto";
-
 import { User } from "../models/user.model.js";
 import { generateVerificationToken } from "../utils/generareVerificationToken.js";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
